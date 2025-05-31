84ab818b2844013159fc7230a9c606f1735f9ad6
Igor Silva <igor.silva@neppo.com.br>
2025-05-31 12:27:35 -0300

    feat: add validation

--- src/main/java/br/com/igorsilva/taskmanager/dtos/TaskDto.java
+++ src/main/java/br/com/igorsilva/taskmanager/dtos/TaskDto.java
@@ -1,7 +1,9 @@
 package br.com.igorsilva.taskmanager.dtos;
 
-import org.antlr.v4.runtime.misc.NotNull;
 
-public record TaskDto(@NotNull String title, String description, String status) {
+import br.com.igorsilva.taskmanager.enuns.Status;
+import jakarta.validation.constraints.NotNull;
+
+public record TaskDto(@NotNull String title, String description, @NotNull Status status) {
 
 }
---
+++ src/main/java/br/com/igorsilva/taskmanager/enuns/Status.java
@@ -0,0 +1,8 @@
+package br.com.igorsilva.taskmanager.enuns;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED;
+}
